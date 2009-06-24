0b645ea6360f0287759a60c6dbb093d104dc0b1c
Eloy Duran <eloy.de.enige@gmail.com>
2009-06-25 01:05:18 +0200

    Small cleanup

--- lib/kicker/recipes/could_not_handle_file.rb
+++ lib/kicker/recipes/could_not_handle_file.rb
@@ -1,7 +1,5 @@
 class Kicker
-  COULD_NOT_HANDLE_CALLBACK = lambda do |kicker, files|
+  self.callback = lambda do |kicker, files|
     kicker.log("Could not handle: #{files.join(', ')}")
   end
-  
-  self.callback = COULD_NOT_HANDLE_CALLBACK
 end
