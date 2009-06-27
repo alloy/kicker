c253d6b478980d077378d662253ba483af991007
Eloy Duran <eloy.de.enige@gmail.com>
2009-06-27 02:11:52 +0200

    Use the post process chain for the `could not handle' callback.

--- lib/kicker/recipes/could_not_handle_file.rb
+++ lib/kicker/recipes/could_not_handle_file.rb
@@ -1,4 +1,4 @@
-Kicker.process_callback = lambda do |kicker, files|
+Kicker.post_process_callback = lambda do |kicker, files|
   kicker.log('')
   kicker.log("Could not handle: #{files.join(', ')}")
   kicker.log('')
