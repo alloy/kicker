cabb45c0ac99c90ed65a387011b235c5742f942c
Eloy Duran <eloy.de.enige@gmail.com>
2009-06-28 02:46:47 +0200

    Added Kicker::Recipes::Base class to abstract some cumbersome stuff in setting up a handler class. And started on the Rails handler.

---
+++ lib/kicker/recipes/rails.rb
@@ -0,0 +1,28 @@
+class Kicker
+  module Recipes
+    class Rails < Base
+      attr_reader :test_files
+      
+      def after_initialize
+        @test_files = []
+      end
+      
+      def handle!
+        @files.delete_if do |full_path|
+          path = relative_path(full_path)
+          
+          # Match any ruby test file and run it
+          if path =~ /^test\/.+_test\.rb$/
+            @test_files << path
+          end
+        end
+        
+        run_tests
+      end
+      
+      def run_tests
+        @kicker.execute_command("ruby -r #{@test_files.join(' -r ')} -e ''")
+      end
+    end
+  end
+end
---
+++ test/recipes/rails_test.rb
@@ -0,0 +1,33 @@
+require File.expand_path('../../test_helper', __FILE__)
+require 'kicker/recipes/rails'
+
+describe "The Kicker::Recipes::Rails handler" do
+  before do
+    @kicker = Kicker.new({})
+    @kicker.stubs(:execute_command)
+    
+    @handler = Kicker::Recipes::Rails.new(@kicker, [])
+  end
+  
+  it "should instantiate a new instance and call the #handle! method when called" do
+    files = %w{}
+    instance = mock('Kicker::Recipes::Rails')
+    instance.expects(:handle!)
+    
+    Kicker::Recipes::Rails.expects(:new).with(@kicker, files).returns(instance)
+    Kicker::Recipes::Rails.call(@kicker, files)
+  end
+  
+  it "should match, extract, and run any test case files that have changed" do
+    lib_file = File.expand_path('lib/foo.rb')
+    @handler.files << lib_file
+    @handler.files << File.expand_path('test/1_test.rb')
+    @handler.files << File.expand_path('test/namespace/2_test.rb')
+    
+    @kicker.expects(:execute_command).with("ruby -r test/1_test.rb -r test/namespace/2_test.rb -e ''")
+    @handler.handle!
+    
+    @handler.test_files.should == %w{ test/1_test.rb test/namespace/2_test.rb }
+    @handler.files.should == [lib_file]
+  end
+end
