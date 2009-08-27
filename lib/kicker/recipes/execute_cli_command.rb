b502c8e698b817cf9f0261400918107c2b0ebc82
Cristi Balan <evil@che.lu>
2009-08-27 15:24:03 +0300

    Give more precedence to --execute so it runs even when you have a .kick file

--- lib/kicker/recipes/execute_cli_command.rb
+++ lib/kicker/recipes/execute_cli_command.rb
@@ -1,5 +1,5 @@
 Kicker.option_parser.on('-e', '--execute [COMMAND]', 'The command to execute.') do |command|
-  Kicker.process_callback = lambda do |files|
+  Kicker.pre_process_callback = lambda do |files|
     files.clear
     Kicker.execute_command "sh -c #{command.inspect}"
   end
--- test/recipes/execute_cli_command_test.rb
+++ test/recipes/execute_cli_command_test.rb
@@ -2,19 +2,19 @@ require File.expand_path('../../test_helper', __FILE__)
 
 describe "Kicker, concerning the `execute a command-line' callback" do
   it "should parse the command and add the callback" do
-    before = Kicker.process_chain.length
+    before = Kicker.pre_process_chain.length
     
     Kicker.parse_options(%w{ -e ls })
-    Kicker.process_chain.length.should == before + 1
+    Kicker.pre_process_chain.length.should == before + 1
     
     Kicker.parse_options(%w{ --execute ls })
-    Kicker.process_chain.length.should == before + 2
+    Kicker.pre_process_chain.length.should == before + 2
   end
   
   it "should call execute_command with the given command" do
     Kicker.parse_options(%w{ -e ls })
     
-    callback = Kicker.process_chain.last
+    callback = Kicker.pre_process_chain.last
     callback.should.be.instance_of Proc
     
     Kicker.expects(:execute_command).with('sh -c "ls"')
@@ -26,7 +26,7 @@ describe "Kicker, concerning the `execute a command-line' callback" do
     Kicker.stubs(:execute_command)
     
     files = %w{ /file/1 /file/2 }
-    Kicker.process_chain.last.call(files)
+    Kicker.pre_process_chain.last.call(files)
     files.should.be.empty
   end
 end
