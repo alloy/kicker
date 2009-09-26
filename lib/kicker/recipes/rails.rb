4fe45f275709d71338c99dfa37826e87b9cfedb0
Eloy Duran <eloy.de.enige@gmail.com>
2009-09-27 01:51:55 +0200

    Refactored a bit.

--- lib/kicker/recipes/rails.rb
+++ lib/kicker/recipes/rails.rb
@@ -1,4 +1,15 @@
 module Rails
+  def self.type_to_test_dir(type)
+    case type
+    when "models"
+      "unit"
+    when "concerns"
+      "unit/concerns"
+    when "controllers", "views"
+      "functional"
+    end
+  end
+  
   def self.all_functional_tests
     Dir.glob("test/functional/**/*_test.rb")
   end
@@ -8,33 +19,25 @@ process do |files|
   test_files = []
   
   files.delete_if do |file|
+    case file
     # Match any ruby test file and run it
-    if file =~ /^test\/.+_test\.rb$/
+    when /^test\/.+_test\.rb$/
       test_files << file
     
     # Run all functional tests when routes.rb is saved
-    elsif file == 'config/routes.rb'
+    when 'config/routes.rb'
       files.delete(file)
       test_files.concat Rails.all_functional_tests
     
     # Match lib/*
-    elsif file =~ /^(lib\/.+)\.rb$/
+    when /^(lib\/.+)\.rb$/
       test_files << "test/#{$1}_test.rb"
     
     # Match any file in app/ and map it to a test file
-    elsif match = file.match(%r{^app/(\w+)([\w/]*)/([\w\.]+)\.\w+$})
-      type, namespace, file = match[1..3]
+    when %r{^app/(\w+)([\w/]*)/([\w\.]+)\.\w+$}
+      type, namespace, file = $1, $2, $3
       
-      dir = case type
-      when "models"
-        "unit"
-      when "concerns"
-        "unit/concerns"
-      when "controllers", "views"
-        "functional"
-      end
-      
-      if dir
+      if dir = Rails.type_to_test_dir(type)
         if type == "views"
           namespace = namespace.split('/')[1..-1]
           file = "#{namespace.pop}_controller"
