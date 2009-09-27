ce785552d918d23b8dba75cba6b9102f9d4e4381
Eloy Duran <eloy.de.enige@gmail.com>
2009-09-27 18:32:31 +0200

    Also flatten and compact the result array of Array#take_and_map by default.

--- lib/kicker/core_ext.rb
+++ lib/kicker/core_ext.rb
@@ -1,20 +1,26 @@
 class Kicker
   module ArrayExt
     # Deletes elements from self for which the block evaluates to +true+. A new
-    # array is returned with those values the block returned.
-    #
-    # Basically, a combination of reject! and map.
+    # array is returned with those values the block returned. So basically, a
+    # combination of reject! and map.
     #
     #   a = [1,2,3]
     #   b = a.take_and_map { |x| x * 2 if x == 2 }
     #   b # => [4]
     #   a # => [1, 3]
-    def take_and_map
+    #
+    # If +flatten_and_compact+ is +true+, the result array will be flattened
+    # and compacted. The default is +true+.
+    def take_and_map(flatten_and_compact = true)
       took = []
       reject! do |x|
         if result = yield(x)
           took << result
         end
+      end
+      if flatten_and_compact
+        took.flatten!
+        took.compact!
       end
       took
     end
--- test/core_ext_test.rb
+++ test/core_ext_test.rb
@@ -13,4 +13,16 @@ describe "Array#take_and_map" do
   it "should return a new array of the return values of each block call that evaluates to true" do
     @array.take_and_map { |x| $1 if x =~ /^ba(\w)/ }.should == %w{ r z }
   end
+  
+  it "should flatten and compact the result array" do
+    @array.take_and_map do |x|
+      x =~ /^ba/ ? %w{ f o o } : [nil]
+    end.should == %w{ f o o f o o }
+  end
+  
+  it "should not flatten and compact the result array if specified" do
+    @array.take_and_map(false) do |x|
+      x =~ /^ba/ ? %w{ f o o } : [nil]
+    end.should == [[nil], %w{ f o o }, %w{ f o o }]
+  end
 end
