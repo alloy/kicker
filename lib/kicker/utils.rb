f195868e692be88f1971f1df145fd6fde2e683f5
Manfred Stienstra <manfred@fngtps.com>
2009-11-01 15:31:19 +0100

    Add a ruby_bin_path to Kicker::Utils.
    By default Kicker uses 'ruby' to run command. Sometimes you want to run a
    different ruby binary for some reason. This will enable the binary to set
    an other ruby binary path.

--- lib/kicker/utils.rb
+++ lib/kicker/utils.rb
@@ -2,6 +2,9 @@ class Kicker
   module Utils #:nodoc:
     extend self
     
+    attr_accessor :ruby_bin_path
+    self.ruby_bin_path = 'ruby'
+    
     def execute(command)
       @last_command = command
       
@@ -32,7 +35,7 @@ class Kicker
     end
     
     def run_ruby_tests(tests)
-      execute "ruby -r #{tests.join(' -r ')} -e ''" unless tests.empty?
+      execute "#{ruby_bin_path} -r #{tests.join(' -r ')} -e ''" unless tests.empty?
     end
     
     private
